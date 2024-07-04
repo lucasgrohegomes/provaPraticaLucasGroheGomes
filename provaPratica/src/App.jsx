722dc88f79de0f1b27fa15e0791ef27cda0b55bd
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2024-07-04 12:08:47 -0300

    entrega

---
+++ provaPratica/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Clubs from './components/Clubs';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Clubs />
+    </div>
+  );
+}
+
+export default App;
---
+++ provaPratica/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <div>
+        <img src="https://logodownload.org/wp-content/uploads/2019/08/senai-logo-1.png" className='img-senai'/>
+      </div>
+      <header>
+        <h1>Lucas Grohe Gomes - 3°C</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
